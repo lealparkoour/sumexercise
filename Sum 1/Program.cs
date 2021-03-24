176eb2aaebdce90363ed852ceaeb768972a22e36
sebastian leal <ghostsebas@hotmail.com>
2021-03-24 00:15:54 -0500

    1

---
+++ Sum 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sum_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n ;
+            Console.Write("enter the number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(recurSum(n));
+
+
+
+        }
+         static int recurSum(int n)
+        {
+            if (n <= 1)
+                return n;
+            return n + recurSum(n - 1);
+        }
+    }
+}
